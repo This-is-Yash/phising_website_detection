b352251a361351e33cce83e11fef7ac4bb438989
This-is-Yash <152937823+This-is-Yash@users.noreply.github.com>
2026-03-20 23:40:52 +0530

    Update run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-echo "🚀 Starting FastAPI URL Legitimacy Checker..."
-exec uvicorn app:app --host 0.0.0.0 --port 8080
+cd backend
+docker build -t url-checker .
+docker run -it --rm -p 8080:8080 url-checker
